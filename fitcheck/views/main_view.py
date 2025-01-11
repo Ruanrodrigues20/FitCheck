0d4d91fa23dc6d638244691d9e92e97c5165814d
ruan-rodrigues <ruan-rodrigues@pop-os.intelbras.local>
2025-01-11 23:17:19 +0000

    up

--- fitcheck/views/main_view.py
+++ fitcheck/views/main_view.py
@@ -1,9 +1,9 @@
-'''from fitcheck.controllers.main_controller import MainController
+from fitcheck.controllers.main_controller import MainController
 
 
 def terminal_interface():
     controller = MainController()
-    
+
     while True:
         print("\n--- FitCheck Terminal Interface ---")
         print("1. Verificar se personal está registrado")
@@ -15,20 +15,20 @@ def terminal_interface():
         print("7. Mostrar avaliações de uma pessoa")
         print("8. Gerar relatório em PDF")
         print("0. Sair")
-        
+
         option = input("Escolha uma opção: ")
-        
+
         if option == "1":
             if controller.is_personal():
                 print("Personal já registrado.")
             else:
                 print("Nenhum personal registrado.")
-        
+
         elif option == "2":
             name = input("Digite o nome do personal: ")
             controller.add_personal(name)
             print(f"Personal '{name}' registrado com sucesso.")
-        
+
         elif option == "3":
             name = input("Digite o nome da pessoa: ")
             birth_year = int(input("Digite o ano de nascimento: "))
@@ -36,7 +36,7 @@ def terminal_interface():
             gender = input("Digite o gênero (M/F): ")
             person_id = controller.add_person(name, birth_year, height, gender)
             print(f"Pessoa adicionada com ID: {person_id}")
-        
+
         elif option == "4":
             people = controller.show_people()
             if not people:
@@ -45,12 +45,12 @@ def terminal_interface():
                 print("--- Lista de Pessoas ---")
                 for person in people:
                     print(person)
-        
+
         elif option == "5":
             person_id = int(input("Digite o ID da pessoa a ser removida: "))
             controller.remove_person(person_id)
             print(f"Pessoa com ID {person_id} removida.")
-        
+
         elif option == "6":
             person_id = int(input("Digite o ID da pessoa: "))
             print("Insira os dados da avaliação:")
@@ -63,7 +63,7 @@ def terminal_interface():
                 kwargs[key] = value
             controller.add_evaluation_in_person(person_id, **kwargs)
             print("Avaliação adicionada com sucesso.")
-        
+
         elif option == "7":
             person_id = int(input("Digite o ID da pessoa: "))
             evaluations = controller.show_evaluation_in_person(person_id)
@@ -73,7 +73,7 @@ def terminal_interface():
                 print("--- Avaliações ---")
                 for evaluation in evaluations:
                     print(evaluation)
-        
+
         elif option == "8":
             person_id = int(input("Digite o ID da pessoa: "))
             eval1 = int(input("Digite o nome da primeira avaliação: "))
@@ -84,15 +84,15 @@ def terminal_interface():
                 eval2 = None
             controller.create_report_pdf(person_id, eval1, eval2)
             print("Relatório PDF gerado com sucesso.")
-        
+
         elif option == "0":
             print("Saindo do programa...")
             break
-        
+
         else:
             print("Opção inválida. Tente novamente.")
 
 
 if __name__ == "__main__":
     terminal_interface()
-'''
+
